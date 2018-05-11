3f93fda67dff441abce9e0b20dbcbac075282ebd
Bart Laga <bartosz.laga@freshfields.com>
2018-05-11 10:55:59 +0000

    Correction of inclued statement

---
+++ modules/server_host/manifest/init.pp
@@ -0,0 +1,9 @@
+# == Class: class_name
+#
+class server_host {
+  host { 'puppetserver':
+    ensure => 'present',
+    name   => ['puppet-fundamentals-server-10.local', 'puppet-fundamentals-server-10'],
+    ip     => '159.65.85.160',
+  }
+}
